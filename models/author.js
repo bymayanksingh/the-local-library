ec41ec2581c598090b43180ee57997cfb9c51f12
code-monk08 <mayank.singh081997@gmail.com>
2019-11-16 00:37:32 +0530

    created book, bookinstance, genre model

--- models/author.js
+++ models/author.js
@@ -1,8 +1,8 @@
-var mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-var Schema = mongoose.Schema
+const Schema = mongoose.Schema
 
-var AuthorSchema = new Schema({
+const AuthorSchema = new Schema({
   first_name: {
     type: String,
     required: true,
