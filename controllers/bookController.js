668ad41331e3ea490487c12daede517c8fa5158f
code-monk08 <mayank.singh081997@gmail.com>
2020-01-27 03:49:51 +0530

    added ui in pug

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,10 +1,32 @@
 var Book = require('../models/book')
+var Author = require('../models/author')
+var Genre = require('../models/genre')
+var BookInstance = require('../models/bookinstance')
+
+var async = require('async')
 
 //Index page
 exports.index = function (req, res) {
-  res.send('NOT IMPLEMENTED: SITE HOME PAGE')
+  async.parallel({
+    book_count: function (callback) {
+      Book.countDocuments({}, callback)
+    },
+    book_instance_count: function (callback) {
+      BookInstance.countDocuments({}, callback)
+    },
+    book_instance_available_count: function (callback) {
+      BookInstance.countDocuments({ status: 'Available' }, callback)
+    },
+    author_count: function (callback) {
+      Author.countDocuments({}, callback)
+    },
+    genre_count: function (callback) {
+      Genre.countDocuments({}, callback)
+    }
+  }, function (err, results) {
+    res.render('index', { title: 'Local Library Home', error: err, data: results })
+  })
 }
-
 // Display list of all Books
 exports.book_list = function (req, res) {
   res.send('NOT IMPLEMENTED: Book LIST')
